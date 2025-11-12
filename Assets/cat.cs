bf47c06a8f6ce5f1330b3f38934636cfe3ec7a47
TwilightEdge <109018713+TwilightEdge@users.noreply.github.com>
2025-11-12 13:21:09 +0330

    dash added

--- Assets/cat.cs
+++ Assets/cat.cs
@@ -6,19 +6,51 @@ public class cat : MonoBehaviour
     
     Rigidbody2D rigidBodyReference;
     
-    float speed = 0.1f;
+    public float speed = 0.05f;
+    public float dashForce = 1000f;
+    
+    private Vector2 movementInput;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidBodyReference = gameObject.GetComponent<Rigidbody2D>();
+        //rigidBodyReference = GetComponent<Rigidbody2D>();
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBodyReference.AddForce(movementInput * dashForce, ForceMode2D.Impulse);
+            //rigidBodyReference.AddForce(Vector3.up * dashForce);
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Vector3 currentPosition = transform.position;
+            //currentPosition.x += dashSpeed;
         
-        if (Input.GetKeyDown(KeyCode.D))
+            //transform.position = currentPosition;
+            
+            
+            //rigidBodyReference.AddForce(movementInput * dashForce, ForceMode2D.Impulse);
+            rigidBodyReference.AddForce(new Vector2(1, 0) * dashForce, ForceMode2D.Impulse);
+            
+            //rigidBodyReference.linearVelocity = Vector3.zero;
+            //rigidBodyReference.AddForce(Vector3.up * force);
+            
+            //transform.rotation = Quaternion.Euler(0, 0, 35f);
+            //audioSource.PlayOneShot(jumpSound);
+        }*/
+        
+        if (Input.GetKey(KeyCode.D))
         {
             Vector3 currentPosition = transform.position;
             currentPosition.x += speed;
@@ -28,9 +60,51 @@ public class cat : MonoBehaviour
             //rigidBodyReference.linearVelocity = Vector3.zero;
             //rigidBodyReference.AddForce(Vector3.up * force);
             
-            //transform.rotation = Quaternion.Euler(0, 0, 35f);
+            movementInput= new Vector2(1, 0);
+            
+            transform.rotation = Quaternion.Euler(0, 180, 0);
             //audioSource.PlayOneShot(jumpSound);
         }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.x -= speed;
+
+            transform.position = currentPosition;
+            
+            movementInput= new Vector2(-1, 0);
+            
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            
+            
+            
+        }
         
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.y += speed;
+
+            transform.position = currentPosition;
+            
+            movementInput= new Vector2(0, 1);
+            
+            //transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+        
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.y -= speed;
+
+            transform.position = currentPosition;
+            
+            movementInput= new Vector2(0, -1);
+            
+            //transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+
     }
 }
