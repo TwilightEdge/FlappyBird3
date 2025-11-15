92ff11bdf8d30bca3cea37f38e6dd970bbfeed43
TwilightEdge <109018713+TwilightEdge@users.noreply.github.com>
2025-11-15 14:33:27 +0330

    finilized edward scene and prefabs and problem of the emote, edward can now start working on the scene more

--- Assets/scripts/human.cs
+++ Assets/scripts/human.cs
@@ -40,6 +40,8 @@ public class human : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        cat = GameObject.FindWithTag("Player");
+        
         classcat = cat.GetComponent<cat>();
         
         clock = GameObject.FindWithTag("clock");
