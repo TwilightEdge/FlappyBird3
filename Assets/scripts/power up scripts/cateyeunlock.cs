b92c90fa81293660a6c18d16cd0e087bc35b095c
TwilightEdge <109018713+TwilightEdge@users.noreply.github.com>
2025-11-16 14:54:25 +0330

    you now can unlock run and cat eye and kockback and humans req cuteness resets everyday

---
+++ Assets/scripts/power up scripts/cateyeunlock.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class cateyeunlock : powerup
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.E) && playerIsTouching)
+        {
+            
+            if (classcat.food >= price)
+            {
+                Debug.Log("you have the food");
+
+                classcat.food -= price;
+
+                this.ApplyEffect();
+
+                
+
+                classshop.UnregisterPowerup(this.gameObject);
+
+                Destroy(gameObject);
+
+            }
+            else
+            {
+                Debug.Log("you do not have enough money");
+                //you do not have enough money
+            }
+            
+            
+        }
+        
+    }
+
+    public override void ApplyEffect()
+    {
+        classcat.catEyeUpgrade = true;
+
+    }
+    
+}
