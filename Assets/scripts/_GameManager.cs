226a23fe3c719799c98700eefdbcf6c75c7d8a68
TwilightEdge <109018713+TwilightEdge@users.noreply.github.com>
2025-11-16 16:47:04 +0330

    UI WORKS YIPPIEEEEEEEE

--- Assets/scripts/_GameManager.cs
+++ Assets/scripts/_GameManager.cs
@@ -1,7 +1,39 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI HP;
+    public TextMeshProUGUI food;
+    public TextMeshProUGUI cuteness;
+
+    
+    
+    public GameObject cat;
+    
+    public cat classcat;
+
+    void Awake()
+    {
+        cat = GameObject.FindWithTag("Player");
+        
+        classcat = cat.GetComponent<cat>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        HP.text = classcat.HP.ToString();
+        
+        food.text = classcat.food.ToString();
+        
+        cuteness.text = classcat.cuteness.ToString();
+        
+        
+
+
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
