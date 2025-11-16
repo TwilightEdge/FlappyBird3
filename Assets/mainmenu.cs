9d1e3cecc0bb1b4f0f3956566d875f722d4167c8
TwilightEdge <109018713+TwilightEdge@users.noreply.github.com>
2025-11-16 17:02:03 +0330

    mainmenu script added

---
+++ Assets/mainmenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class mainmenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("background");
+    }
+    
+}
